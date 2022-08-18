c6ba70d60ed6a99d58226b5f64fb240dac7fe0de
Fabio Mingo <fabio020mingo@gmail.com>
2022-08-18 18:59:54 -0400

    Ejercicio Terminado

---
+++ src/main/java/paquete1/Main.java
@@ -0,0 +1,19 @@
+package paquete1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static float productomasIVA(float producto){
+        float IVA = 0.16f;
+        float result = producto*IVA;
+        return result;
+    }
+    public static void main(String[] args) {
+        Scanner escaner = new Scanner(System.in);
+        System.out.print("Digite el valor de tu producto :");
+        float valorProducto = escaner.nextFloat();
+        System.out.println("El valor del Producto mas el IVA es :"+ productomasIVA(valorProducto));
+
+
+    }
+}
